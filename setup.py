ac01633a82131ef46216306e25b944420ac47957
Ambadipudi, Manikanta <ma185307@ncr.com>
2020-09-20 13:15:02 +0530

    Committing all the changes needed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='npipes',
+    version='1.0.0',
+    description='Wrapper Library for Named Pipes',
+    author='Manikanta Ambadipudi',
+    author_email='ambadipudi.manikanta@gmail.com',
+    url='https://github.com/mani-src/npipes.git',
+    packages=find_packages()
+)
