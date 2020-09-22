0cee916672f6d3cee09a6db5cad4f11d145bd0a1
Ambadipudi, Manikanta <ma185307@ncr.com>
2020-09-22 11:29:11 +0530

    Modified the file to include long_description and also pointed the URL at the ReadMe

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ setup(
     version='1.0.0',
     license='GPL-3.0',
     description='Wrapper Library for Named Pipes',
+    long_description='Provides a ready to use set of APIs to create and communicate via Named Pipes on Windows, '
+                     'based on win32pipes',
     author='Manikanta Ambadipudi',
     author_email='ambadipudi.manikanta@gmail.com',
-    url='https://github.com/mani-src/npipes.git',
+    url='https://github.com/mani-src/npipes/blob/master/README.md',
     download_url='https://github.com/mani-src/npipes/archive/v1.0.tar.gz',
     install_requires=['pywin32'],
     packages=find_packages()
